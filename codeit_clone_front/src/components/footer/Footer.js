4797350d2589f9e97ecf263d90fdb194bbf228c1
Cheolhyeon Park <fepanbr@gmail.com>
2020-10-06 23:49:32 +0900

    [모듈추가] sass적용
    - package.json에 noe-sass ^4.11.1추가
    - Footer.css를 Footer.scss로 변경

--- codeit_clone_front/src/components/footer/Footer.js
+++ codeit_clone_front/src/components/footer/Footer.js
@@ -1,17 +1,23 @@
 import React from "react";
-import blogImage from "../../assets/sns-blog.png";
-import "./Footer.css";
+import blogImage from "../../assets/image/sns-blog.png";
+import "./Footer.scss";
 
-function Footer() {
+const Footer = () => {
   return (
     <>
-      <footer>
-        <div className="footer-container">
-          <img className="footer-icon" src={blogImage} alt="blog-img"></img>
+      <footer className="footer">
+        <div className="footer__container">
+          <div className="footer__container__text">
+            <span className="footer__text__contents-body">(주) 코드잇</span>
+            <span className="footer__text__contents-tail">|</span>
+          </div>
+          <div className="footer__container__icons">
+            <img className="footer-icon" src={blogImage} alt="blog-img"></img>
+          </div>
         </div>
       </footer>
     </>
   );
-}
+};
 
 export default Footer;
