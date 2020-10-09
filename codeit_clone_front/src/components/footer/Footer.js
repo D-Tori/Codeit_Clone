c8891f63ee2a6a891ae9a343a46bb4a2b7f24029
Cheolhyeon Park <fepanbr@gmail.com>
2020-10-09 17:54:07 +0900

    전체 적용할 css속성 적용 및 footer class 구조 잡기
    - index.css 에 margin padding 속성 추가
    - footer className을 BEM방식을 고려하여 추가

--- codeit_clone_front/src/components/footer/Footer.js
+++ codeit_clone_front/src/components/footer/Footer.js
@@ -1,5 +1,5 @@
 import React from "react";
-import blogImage from "../../assets/image/sns-blog.png";
+import blogImage from "../../assets/image/icons/sns-blog.png";
 import "./Footer.scss";
 
 const Footer = () => {
@@ -7,12 +7,68 @@ const Footer = () => {
     <>
       <footer className="footer">
         <div className="footer__container">
-          <div className="footer__container__text">
-            <span className="footer__text__contents-body">(주) 코드잇</span>
-            <span className="footer__text__contents-tail">|</span>
+          <div className="footer__texts">
+            <div className="footer__texts__row">
+              <span className="text-body">(주) 코드잇</span>
+              <span className="text-tail">|</span>
+              <span className="text-body">Kakao ID코드잇</span>
+              <span className="text-tail">|</span>
+              <span className="text-body">개인정보보호책임자강영훈</span>
+              <span className="text-tail">|</span>
+              <span className="text-body">사업자 번호313-86-00797</span>
+            </div>
           </div>
-          <div className="footer__container__icons">
-            <img className="footer-icon" src={blogImage} alt="blog-img"></img>
+          <div className="footer__icons">
+            <ul className="footer__icons__list--style-none">
+              <li className="footer__icons__item">
+                <a
+                  className="icon-anchor"
+                  href="https://www.youtube.com/channel/UCCM79CPm2WbBYTRaiNEExbg"
+                >
+                  <img className="icon" src={blogImage} alt="blogImage"></img>
+                </a>
+              </li>
+              <li className="footer__icons__item">
+                <a
+                  className="icon-anchor"
+                  href="https://www.youtube.com/channel/UCCM79CPm2WbBYTRaiNEExbg"
+                >
+                  <img className="icon" src={blogImage} alt="blogImage"></img>
+                </a>
+              </li>
+              <li className="footer__icons__item">
+                <a
+                  className="icon-anchor"
+                  href="https://www.youtube.com/channel/UCCM79CPm2WbBYTRaiNEExbg"
+                >
+                  <img className="icon" src={blogImage} alt="blogImage"></img>
+                </a>
+              </li>
+              <li className="footer__icons__item">
+                <a
+                  className="icon-anchor"
+                  href="https://www.youtube.com/channel/UCCM79CPm2WbBYTRaiNEExbg"
+                >
+                  <img className="icon" src={blogImage} alt="blogImage"></img>
+                </a>
+              </li>
+              <li className="footer__icons__item">
+                <a
+                  className="icon-anchor"
+                  href="https://www.youtube.com/channel/UCCM79CPm2WbBYTRaiNEExbg"
+                >
+                  <img className="icon" src={blogImage} alt="blogImage"></img>
+                </a>
+              </li>
+              <li className="footer_icons__item">
+                <a
+                  className="icon-anchor"
+                  href="https://www.youtube.com/channel/UCCM79CPm2WbBYTRaiNEExbg"
+                >
+                  <img className="icon" src={blogImage} alt="blogImage"></img>
+                </a>
+              </li>
+            </ul>
           </div>
         </div>
       </footer>
