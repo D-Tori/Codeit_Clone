f6747dcc7676b18d6a6ce029bb3730e15e13a620
Cheolhyeon Park <fepanbr@gmail.com>
2020-10-25 22:54:07 +0900

    기능변경: DefaultHomeFeature이름 변경 및 경로 변경
    - DefaultHomeFeature.js 이름을 DefaultHomeTwo로 변경

--- codeit_clone_front/src/components/default-home/default-home-feature-two/DefaultHomeFeatureTwo.js
+++ codeit_clone_front/src/components/default-home/default-home-feature-two/DefaultHomeFeatureTwo.js
@@ -1,21 +1,21 @@
 import React from 'react'
 import featureImg from "../../../assets/image/defaultHome/feature3__img2.png"
-import "./DefaultHomeFeature.scss"
+import "./DefaultHomeFeatureTwo.scss"
 
-const mock = [
-  {
-    title: "학습 데이터 분석을 통해 독보적인 콘텐츠 퀄리티 확보",
-    description: `코드잇은 학생들의 수강 데이터와 레슨별 만족도 등을 분석하여,
-    끊임없이 기존 콘텐츠를 발전시키며 차별적인 콘텐츠를 만들어 나갑니다.`
-  },
-  {
-    title: "다양한 기기에서 코딩 실습 및 수강 기능",
-    description: `코드잇의 강의들은 모바일을 포함한 다양한 기기에서 수강이 가능하며,
-    덕분에 언제 어디서든 재밌고 효과적으로 강의에 몰입할 수 있습니다.`
-  }
-];
+// const mock = [
+//   {
+//     title: "학습 데이터 분석을 통해 독보적인 콘텐츠 퀄리티 확보",
+//     description: `코드잇은 학생들의 수강 데이터와 레슨별 만족도 등을 분석하여,
+//     끊임없이 기존 콘텐츠를 발전시키며 차별적인 콘텐츠를 만들어 나갑니다.`
+//   },
+//   {
+//     title: "다양한 기기에서 코딩 실습 및 수강 기능",
+//     description: `코드잇의 강의들은 모바일을 포함한 다양한 기기에서 수강이 가능하며,
+//     덕분에 언제 어디서든 재밌고 효과적으로 강의에 몰입할 수 있습니다.`
+//   }
+// ];
 
-const DefaultHomeFeature = () => {
+const DefaultHomeFeatureTwo = () => {
   return (
     <>
       <div className="feature">
@@ -36,4 +36,4 @@ const DefaultHomeFeature = () => {
   )
 }
 
-export default DefaultHomeFeature
+export default DefaultHomeFeatureTwo;
