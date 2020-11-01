77a45b59f130ea785f3d6f1513959408523a9151
Cheolhyeon Park <fepanbr@gmail.com>
2020-11-01 20:51:52 +0900

    [기능추가] titlebox에 box-shadow추가 및 className 변경
    - text--one, text--two로 나누어 작성.
    - box-shadow효과 추가

--- codeit_clone_front/src/components/default-home/default-home-feature-two/DefaultHomeFeatureTwo.js
+++ codeit_clone_front/src/components/default-home/default-home-feature-two/DefaultHomeFeatureTwo.js
@@ -1,6 +1,6 @@
-import React from 'react'
-import featureImg from "../../../assets/image/defaultHome/feature3__img2.png"
-import "./DefaultHomeFeatureTwo.scss"
+import React from "react";
+import featureImg from "../../../assets/image/defaultHome/feature3__img2.png";
+import "./DefaultHomeFeatureTwo.scss";
 
 // const mock = [
 //   {
@@ -20,20 +20,28 @@ const DefaultHomeFeatureTwo = () => {
     <>
       <div className="feature">
         <div className="feature__container">
-        <div className="feature__text">
-          <div className="title-border">
-            <p className="title"><b>학습 데이터 분석</b>을 통해 독보적인 콘텐츠 퀄리티 확보</p>
+          <div className="feature__text">
+            <div className="title-border--two">
+              <p className="title">
+                <b>학습 데이터 분석</b>을 통해 독보적인 콘텐츠 퀄리티 확보
+              </p>
+            </div>
+            <div className="description">
+              <p>
+                코드잇은 학생들의 <b>수강 데이터와 레슨별 만족도 등을 분석</b>
+                하여,
+              </p>
+              <p>
+                끊임없이 기존 콘텐츠를 발전시키며 차별적인 콘텐츠를 만들어
+                나갑니다.
+              </p>
+            </div>
           </div>
-          <div className="description">
-            <p>코드잇은 학생들의 <b>수강 데이터와 레슨별 만족도 등을 분석</b>하여,</p>
-            <p>끊임없이 기존 콘텐츠를 발전시키며 차별적인 콘텐츠를 만들어 나갑니다.</p>
-          </div>
-        </div>
           <img className="feature__image" src={featureImg} alt="특징 이미지" />
         </div>
       </div>
     </>
-  )
-}
+  );
+};
 
 export default DefaultHomeFeatureTwo;
